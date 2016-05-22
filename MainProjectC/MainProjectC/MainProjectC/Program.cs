c6978afb67cbde344c21cd437e1a5bdcf3985952
Tarveen <tarveen.hanspal@netspi.com>
2016-05-21 23:22:06 -0500

    test

--- MainProjectC/MainProjectC/MainProjectC/Program.cs
+++ MainProjectC/MainProjectC/MainProjectC/Program.cs
@@ -15,7 +15,7 @@ namespace MainProjectC
 		static void Main()
 		{
 			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault( false );
+			Application.SetCompatibleTextRenderingDefault( false );//
 			Application.Run( new Form1() );
 		}
 	}
